b6c8290cddeaa838978dde07d6476d950db886d7
Saqer Aljabri <aljabrisaqer23@gmail.com>
2024-11-22 20:41:19 +0300

    .......

--- lib/login.dart
+++ lib/login.dart
@@ -120,6 +120,147 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
             ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegisterScreen()),
+                );
+              },
+              child: const Text(
+                'إنشاء حساب جديد',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.teal,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterScreen extends StatelessWidget {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+
+  RegisterScreen({super.key});
+
+  Future<void> register(BuildContext context, String username, String password,
+      String email) async {
+    final url = Uri.parse('https://drag-opal.vercel.app/api/User');
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({'username': username, 'password': password}),
+      );
+      if (response.statusCode == 201) {
+        // Handle success and navigate to LoginScreen
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginScreen()),
+        );
+      } else {
+        // Handle error
+        print('Registration failed: ${response.body}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 20),
+            Image.asset(
+              'images/project picture1.jpg', // Add your image asset here
+              height: 200,
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              'إنشاء حساب جديد',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'يرجى ملء البيانات لإنشاء حساب جديد',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 40),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'اسم المستخدم',
+                hintText: 'يرجى إدخال اسم المستخدم',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'البريد الإلكتروني',
+                hintText: 'يرجى إدخال إيميلك هنا',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'كلمة السر',
+                hintText: 'كلمة السر',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                register(context, _usernameController.text,
+                    _passwordController.text, _emailController.text);
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                backgroundColor: Colors.teal,
+              ),
+              child: const Text(
+                'إنشاء حساب',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
           ],
         ),
       ),
